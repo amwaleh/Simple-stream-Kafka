2cc250677f51a878cf19e8d168bf238297307960
alex mwaleh <alex.mwaleh@andela.com>
2017-02-01 15:09:36 +0300

    Add consumer group

--- consumer.py
+++ consumer.py
@@ -3,7 +3,7 @@
 from kafka import KafkaConsumer
 
 # initialise our connection to Kafka server and pass the topic we want from it
-consumer = KafkaConsumer('my-topic', group_id='my_group', bootstrap_servers=['0.0.0.0:9092'])
+consumer = KafkaConsumer('pulse-topic', group_id='my_group', bootstrap_servers=['0.0.0.0:9092'])
 
 # Conitiously listen to the connection and print messages as recieved
 for msg in consumer:
